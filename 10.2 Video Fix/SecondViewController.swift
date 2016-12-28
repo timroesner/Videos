6a8c3304fbfd9cd8d5f291d2919bd0dbe50ffb13
Tim Roesner <Tim@tims-mbp-2.fritz.box>
2016-12-28 12:11:17 +0100

    Initial Commit

---
+++ 10.2 Video Fix/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  10.2 Video Fix
+//
+//  Created by Tim Roesner on 12/28/16.
+//  Copyright © 2016 Tim Roesner. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
