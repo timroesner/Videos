0eec6273b04e05c7b861f8fe67485a9c2b984656
timroesner <13894518+timroesner@users.noreply.github.com>
2018-08-31 23:18:51 -0700

    Create MovieOverviewCollectionViewCell

---
+++ 10.2 Video Fix/Movie/MovieOverviewCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieOverviewCollectionViewCell.swift
+//  10.2 Video Fix
+//
+//  Created by Tim Roesner on 8/30/18.
+//  Copyright © 2018 Tim Roesner. All rights reserved.
+//
+
+import UIKit
+
+class MovieOverviewCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    
+}
