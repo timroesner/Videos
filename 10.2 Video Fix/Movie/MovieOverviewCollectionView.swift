fd3aaaca68ebc2aa81026290fc3da86c66fb1b6d
timroesner <13894518+timroesner@users.noreply.github.com>
2018-08-31 23:18:01 -0700

    Add collectionView DataSource methods

--- 10.2 Video Fix/Movie/MovieOverviewCollectionView.swift
+++ 10.2 Video Fix/Movie/MovieOverviewCollectionView.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2018 Tim Roesner. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension MovieOverviewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if movies.isEmpty {
+            collectionView.backgroundView = EmptyView().setup(title: "No Movies", subtitle: "Connect to iTunes to add movies")
+        } else {
+            collectionView.backgroundView = nil
+        }
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "movieCell", for: indexPath) as! MovieOverviewCollectionViewCell
+        let movie = movies[indexPath.row]
+        cell.thumbnail.image = movie.artwork
+        cell.title.text = movie.title
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "movieDetail") as! MoviesDetail
+        vc.currentMovie = movies[indexPath.row]
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+}
