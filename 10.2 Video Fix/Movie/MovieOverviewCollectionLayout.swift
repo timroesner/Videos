db668aa8ac1e817eee6f34a4ca257b44727341da
timroesner <13894518+timroesner@users.noreply.github.com>
2018-08-31 23:18:17 -0700

    Add CollectionViewLayout

--- 10.2 Video Fix/Movie/MovieOverviewCollectionLayout.swift
+++ 10.2 Video Fix/Movie/MovieOverviewCollectionLayout.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2018 Tim Roesner. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension MovieOverviewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let w = (screenWidth-72)/5
+        let h = w*1.67
+        return CGSize(width: w, height: h)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsetsMake(12, 12, 12, 12);
+    }
+}
