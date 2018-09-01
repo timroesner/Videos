d6d8a9d3c54d7fe0039bbb1b69b0f0a3fddc2b38
timroesner <13894518+timroesner@users.noreply.github.com>
2018-08-31 23:17:41 -0700

    Create MovieOverviewController

---
+++ 10.2 Video Fix/Movie/MovieOverviewController.swift
@@ -0,0 +1,46 @@
+//
+//  FirstViewController.swift
+//  10.2 Video Fix
+//
+//  Created by Tim Roesner on 12/28/16.
+//  Copyright © 2016 Tim Roesner. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import AVKit
+
+class MovieOverviewController: UIViewController {
+    
+    @IBOutlet var collectionView: UICollectionView!
+    var movies = [Movie]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if #available(iOS 11.0, *) {
+            self.navigationController?.navigationBar.prefersLargeTitles = true
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        getFiles()
+    }
+    
+    func getFiles() {
+        let documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        do {
+            // Get the directory contents urls (including subfolders urls)
+            let directoryContents = try FileManager.default.contentsOfDirectory(at: documentsUrl, includingPropertiesForKeys: nil, options: [])
+            
+            // Filter files to only include videos
+            movies = Movie().mapURLs(collection: directoryContents.filter{ $0.pathExtension == "mp4" || $0.pathExtension == "m4v" || $0.pathExtension == "mov"})
+            
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        collectionView.reloadData()
+    }
+}
+
