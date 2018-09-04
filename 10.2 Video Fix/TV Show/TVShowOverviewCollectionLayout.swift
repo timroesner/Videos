2b450bed04b2a454904c1eb3294dcafaecea1e7c
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2018-09-03 23:35:59 -0700

    Add dynamic layout for TV Show cell

---
+++ 10.2 Video Fix/TV Show/TVShowOverviewCollectionLayout.swift
@@ -0,0 +1,26 @@
+//
+//  TVShowOverviewCollectionLayout.swift
+//  10.2 Video Fix
+//
+//  Created by Tim Roesner on 9/3/18.
+//  Copyright © 2018 Tim Roesner. All rights reserved.
+//
+
+import UIKit
+
+extension TVShowOverviewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        var w: CGFloat = 0
+        if UIApplication.shared.statusBarOrientation.isLandscape {
+            w = (UIScreen.main.bounds.width-72)/5
+        } else {
+            w = (UIScreen.main.bounds.width-48)/3
+        }
+        let h = w*1.14
+        return CGSize(width: w, height: h)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsetsMake(12, 12, 12, 12);
+    }
+}
