84c1192355a8f7c43a5bc9aba816e2ab60566bf5
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2018-09-03 23:35:20 -0700

    Add TV Show cell

---
+++ 10.2 Video Fix/TV Show/TVShowOverviewCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TVShowOverviewCollectionViewCell.swift
+//  10.2 Video Fix
+//
+//  Created by Tim Roesner on 9/3/18.
+//  Copyright © 2018 Tim Roesner. All rights reserved.
+//
+
+import UIKit
+
+class TVShowOverviewCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var title: UILabel!
+}
