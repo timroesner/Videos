a2cebdbc4a1717d787145be6da216cf51c9ea5b2
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2018-09-03 23:35:06 -0700

    Add TV Show collection view delegate methods

---
+++ 10.2 Video Fix/TV Show/TVShowOverviewCollectionView.swift
@@ -0,0 +1,30 @@
+//
+//  TVShowOverviewCollectionView.swift
+//  10.2 Video Fix
+//
+//  Created by Tim Roesner on 9/3/18.
+//  Copyright © 2018 Tim Roesner. All rights reserved.
+//
+
+import UIKit
+
+extension TVShowOverviewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return shows.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "showCell", for: indexPath) as! TVShowOverviewCollectionViewCell
+        let currentShow = shows[indexPath.row]
+        cell.thumbnail.image = currentShow.artwork
+        cell.title.text = currentShow.title
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let currentShow = shows[indexPath.row]
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "showDetail") as! TVShowDetail
+        vc.currentShow = currentShow
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+}
