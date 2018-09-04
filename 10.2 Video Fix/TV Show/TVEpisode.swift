7442d2564487e8184490125e0f8763c80ba90dca
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2018-09-03 23:34:33 -0700

    Create TV Show model

---
+++ 10.2 Video Fix/TV Show/TVEpisode.swift
@@ -0,0 +1,16 @@
+//
+//  TVEpisode.swift
+//  10.2 Video Fix
+//
+//  Created by Tim Roesner on 9/3/18.
+//  Copyright © 2018 Tim Roesner. All rights reserved.
+//
+
+import UIKit
+
+struct TVEpisode {
+    var title = ""
+    var number = ""
+    var url: URL!
+    var duration = 0
+}
---
+++ 10.2 Video Fix/TV Show/TVShow.swift
@@ -0,0 +1,16 @@
+//
+//  TVShow.swift
+//  10.2 Video Fix
+//
+//  Created by Tim Roesner on 9/3/18.
+//  Copyright © 2018 Tim Roesner. All rights reserved.
+//
+
+import UIKit
+
+struct TVShow {
+    var title = ""
+    var artwork = UIImage(named: "MissingArtworkTV")
+    var description = ""
+    var episodes = [TVEpisode]()
+}
