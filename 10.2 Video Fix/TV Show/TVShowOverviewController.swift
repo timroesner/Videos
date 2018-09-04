0b070729204b32ff5e833673b6c2088612d4e31e
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2018-09-03 23:35:42 -0700

    Modify TV Show overview controller

---
+++ 10.2 Video Fix/TV Show/TVShowOverviewController.swift
@@ -0,0 +1,42 @@
+//
+//  SecondViewController.swift
+//  10.2 Video Fix
+//
+//  Created by Tim Roesner on 12/28/16.
+//  Copyright © 2016 Tim Roesner. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import AVKit
+
+class TVShowOverviewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    var shows = [TVShow]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getFiles()
+    }
+    
+    func getFiles() {
+        let documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        do {
+            // Get the directory contents urls (including subfolders urls)
+            let directoryContents = try FileManager.default.contentsOfDirectory(at: documentsUrl, includingPropertiesForKeys: nil, options: [])
+            
+            // Check for directories
+            shows = TVShow().mapURLs(collection: directoryContents.filter{$0.hasDirectoryPath})
+            
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+}
+
