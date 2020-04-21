a9cfbf714f3710177674ce80fcce0c47bbc6d78d
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2020-04-20 22:19:18 -0700

    Update empty view

--- 10.2 Video Fix/EmptyView.swift
+++ 10.2 Video Fix/EmptyView.swift
@@ -10,35 +10,30 @@ import UIKit
 
 class EmptyView: UIView {
     
-    var title = UILabel()
-    var sub = UILabel()
+    var titleLabel = UILabel()
+    var subtitleLabel = UILabel()
     
     func setup(title:String, subtitle:String) -> UIView {
-        self.title.text = title
-        self.title.font = UIFont.boldSystemFont(ofSize: 36)
-        self.title.textAlignment = .center
-        self.title.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.text = title
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 36)
+        titleLabel.textAlignment = .center
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
         
-        self.sub.text = subtitle
-        self.sub.font = UIFont.preferredFont(forTextStyle: .callout)
-        self.sub.textAlignment = .center
-        self.sub.translatesAutoresizingMaskIntoConstraints = false
+        subtitleLabel.text = subtitle
+        subtitleLabel.font = UIFont.preferredFont(forTextStyle: .callout)
+        subtitleLabel.textAlignment = .center
+        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
         
-        let titleSize = self.title.sizeThatFits(CGSize())
-        let subSize = self.sub.sizeThatFits(CGSize())
+        addSubview(titleLabel)
+        addSubview(subtitleLabel)
         
-        addSubview(self.title)
-        addSubview(self.sub)
-        
-        self.title.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        self.title.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-        self.title.widthAnchor.constraint(equalToConstant: titleSize.width).isActive = true
-        self.title.heightAnchor.constraint(equalToConstant: titleSize.height).isActive = true
-        
-        self.sub.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        self.sub.topAnchor.constraint(equalTo: self.title.bottomAnchor, constant: 6).isActive = true
-        self.sub.widthAnchor.constraint(equalToConstant: subSize.width).isActive = true
-        self.sub.heightAnchor.constraint(equalToConstant: subSize.height).isActive = true
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            
+            subtitleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+        ])
         
         return self
     }
