d8a91be7344fef5a3fcd1aeec0262186d005df85
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2020-06-16 20:48:10 -0700

    Add ability to open external files

--- 10.2 Video Fix/AppDelegate.swift
+++ 10.2 Video Fix/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import AVFoundation
 import AVKit
+import AmplitudeLite
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -47,5 +48,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+	
+	func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+		guard let rootVC = app.keyWindow?.rootViewController else { return false }
+		
+		if let playerVC = rootVC.presentedViewController as? AVPlayerViewController {
+			playerVC.dismiss(animated: false, completion: nil)
+		}
+		
+		rootVC.presentPlayer(with: url)
+		return true
+	}
 }
 
